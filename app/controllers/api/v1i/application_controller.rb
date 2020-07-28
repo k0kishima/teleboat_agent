a66938004c95739465994aea817b9a24e737c38d
Johndoe <56298669+togane@users.noreply.github.com>
2020-07-28 18:13:36 +0900

    fix autorization

--- app/controllers/api/v1i/application_controller.rb
+++ app/controllers/api/v1i/application_controller.rb
@@ -3,18 +3,6 @@ module Api
     class ApplicationController < ActionController::API
       before_action :authenticate_application!
 
-      rescue_from ActiveRecord::RecordNotFound do |e|
-        render status: 404, json: { errors: [{ message: e.message }] }
-      end
-
-      rescue_from ActiveRecord::RecordInvalid  do |e|
-        render status: 400, json: { errors: e.record&.errors }
-      end
-
-      rescue_from ::DataExceptions::DataFixed, DataExceptions::DataFetching do |e|
-        render status: 400, json: { errors: e.message }
-      end
-
       rescue_from ActionController::BadRequest do |e|
         render status: 400, json: { errors: [{ message: e.message }] }
       end
@@ -22,7 +10,7 @@ module Api
       private
 
         def authenticate_application!
-          InternalApi::CheckApplicationTokenService.call(access_token: params[:access_token])
+          ::InternalApi::CheckApplicationTokenService.call(access_token: params[:access_token])
         end
     end
   end
--- app/services/internal_api/check_application_token_service.rb
+++ app/services/internal_api/check_application_token_service.rb
@@ -1,16 +1,18 @@
-class InternalApi::CheckApplicationTokenService
-  include ServiceBase
+module InternalApi
+  class CheckApplicationTokenService
+    include ServiceBase
 
-  def call
-    return true if access_token.in?(access_grants)
-    raise ActionController::BadRequest
-  end
-
-  private
-
-    attr_accessor :access_token
-
-    def access_grants
-      [Rails.application.config.x.application_token]
+    def call
+      return true if access_token.in?(access_grants)
+      raise ActionController::BadRequest
     end
+
+    private
+
+      attr_accessor :access_token
+
+      def access_grants
+        [Rails.application.config.x.application_token]
+      end
+  end
 end
