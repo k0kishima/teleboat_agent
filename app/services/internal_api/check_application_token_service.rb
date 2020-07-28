4eaf8aa00997a1a838506ea2bd61df93bcd0ec4f
Johndoe <56298669+togane@users.noreply.github.com>
2020-07-28 17:59:59 +0900

    implement check application token service

---
+++ app/services/internal_api/check_application_token_service.rb
@@ -0,0 +1,16 @@
+class InternalApi::CheckApplicationTokenService
+  include ServiceBase
+
+  def call
+    return true if access_token.in?(access_grants)
+    raise ActionController::BadRequest
+  end
+
+  private
+
+    attr_accessor :access_token
+
+    def access_grants
+      [Rails.application.config.x.application_token]
+    end
+end
