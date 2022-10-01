48f7323a9c93ef3401ea3cf5d8801331d97aa56b
Johndoe <56298669+togane@users.noreply.github.com>
2022-10-01 10:34:01 +0900

    add selenium options

--- app/services/vote_tickets_service.rb
+++ app/services/vote_tickets_service.rb
@@ -83,6 +83,8 @@ class VoteTicketsService
       options = Selenium::WebDriver::Chrome::Options.new
       options.add_argument('--headless')
       options.add_argument('--no-sandbox')
+      options.add_argument('--disable-dev-shm-usage')
+      options.add_argument('--disable-gpu')
       options.add_argument("--user-agent=#{USER_AGENT}")
       Selenium::WebDriver.for :chrome, options: options
     end
